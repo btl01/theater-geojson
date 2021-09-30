7baf1ee5519ce9ea3c936d8056601d96554934ff
luanbt21 <luanbt21@gmail.com>
2021-09-30 20:56:41 +0700

    get pid

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { urlencoded } = require('express');
 const express = require('express');
 const cors = require('cors');
+const process = require('process');
 
 const connectDB = require('./config/db');
 const theater = require('./routers/theater');
@@ -19,6 +20,10 @@ app.use('/api/theater', theater);
 app.all('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+if (process.pid) {
+  console.log('This process is your pid ' + process.pid);
+}
 
 app.listen(process.env.PORT || 8005, () => {
   console.log(`PORT ${process.env.PORT || 8005}`);
